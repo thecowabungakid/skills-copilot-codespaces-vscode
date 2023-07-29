c73c96abfbbf78251b12c295160c64f518d22dcb
thecowabungakid <ocampog.ma@gmail.com>
2023-07-29 13:05:47 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var m
+
+
+
+}
